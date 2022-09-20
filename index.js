f3308a3557a31344f9af371cbc32b82afba0ce22
Bianca.Genovese <biancagenovese@live.com.au>
2022-09-20 15:49:20 +1000

    First commit

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log('Welcome to the Random Housewifes Generator');
+console.log('You are: ');
+console.log('You are married to: ');
+console.log('Your iconic quote is: ');
+
+
+
+const housewifeNames = ['Kyle', 'Ramona', 'Vicki', 'Luann', 'Sonja', 'Ashley Darby', 'Bethenny', 'Lisa Barlow', 'Garcelle', 'Giselle', 'Karen Huger', 'Jen Shah'];
+
+const iconicQuotes = ['I have never been with multiple partners in my life!', "That's my OPINION", "Go to sleep!", "The morally corrupt Faye Resnick", "is Bitch better?", "Say it, forget it, write it, regret it", "You stole my goddamn house!", "Little girl"];
+
+const houseHusband = ["Mauricio", "pk", "kelsey grammar", "harry hamlin", "tom girardi", "coach shah", "slade", "jason", "ken", "ralph"];
+
